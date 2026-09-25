593096303188b0bf5cf63d653011c63a72f73e78
Tigercrl <tigercrl@icloud.com>
2026-09-25 15:17:47 +0800

    del: temp dir

--- internal/service/storage/storage.go
+++ internal/service/storage/storage.go
@@ -8,13 +8,9 @@ import (
 
 var StorageDir = "./storage"
 var FileDir = filepath.Join(StorageDir, "files")
-var TempDir = filepath.Join(StorageDir, "tmp")
 
 func EnsureDir() error {
 	if err := os.MkdirAll(FileDir, 0o755); err != nil {
-		return fmt.Errorf("storage: create dir: %w", err)
-	}
-	if err := os.MkdirAll(TempDir, 0o755); err != nil {
 		return fmt.Errorf("storage: create dir: %w", err)
 	}
 	return nil
